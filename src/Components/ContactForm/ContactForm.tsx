65247031877db89358b0ee436958487a8bb49715
Goddak <kris@goddak.co.uk>
2023-03-06 12:34:36 +0000

    Make contact form error messages red

--- src/Components/ContactForm/ContactForm.tsx
+++ src/Components/ContactForm/ContactForm.tsx
@@ -21,22 +21,22 @@ const ContactForm: React.FC = () => {
 		<div className="flex flex-col mb-4">
 			<label className="font-light text-xl" htmlFor="name">Name</label>
 			<input className="w-full p-2 border border-solid border-bts-black rounded-md xl:w-96 xl:p-1 xl:pl-2" type="text" {...register('name', { required: true })} />
-			{errors.name && <span>This field is required</span>}
+			{errors.name && <span className="text-bts-red">This field is required</span>}
 		</div>
 		<div className="flex flex-col mb-4">
 			<label className="font-light text-xl" htmlFor="email">Email</label>
 			<input className="w-full p-2 border border-solid border-bts-black rounded-md xl:w-96 xl:p-1 xl:pl-2" type="email" {...register("email", { required: true })} />
-			{errors.email && <span>This field is required</span>}
+			{errors.email && <span className="text-bts-red">This field is required</span>}
 		</div>
 		<div className="flex flex-col mb-4">
 			<label className="font-light text-xl" htmlFor="phone">Phone</label>
 			<input className="w-full p-2 border border-solid border-bts-black rounded-md xl:w-96 xl:p-1 xl:pl-2" type="text" {...register("phone", { required: true })} />
-			{errors.phone && <span>This field is required</span>}
+			{errors.phone && <span className="text-bts-red">This field is required</span>}
 		</div>
 		<div className="flex flex-col mb-4">
 			<label className="font-light text-xl" htmlFor="message">Message</label>
 			<textarea className="w-full p-2 border border-solid border-bts-black rounded-md xl:w-96 xl:p-1 xl:pl-2" {...register("message", { required: true })} />
-			{errors.message && <span>This field is required</span>}
+			{errors.message && <span className="text-bts-red">This field is required</span>}
 		</div>
 		<input type="submit" className="w-full rounded-md bg-bts-yellow text-bts-black p-4" />
 	</form>;
